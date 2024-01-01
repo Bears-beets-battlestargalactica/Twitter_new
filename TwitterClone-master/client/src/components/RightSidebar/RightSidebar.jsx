00a38a8829ad29308e2efb3b8e4efae0a6a3c188
Bears-beets-battlestargalactica <117076305+Bears-beets-battlestargalactica@users.noreply.github.com>
2024-01-01 13:27:08 -0800

    Initial commit

---
+++ TwitterClone-master/client/src/components/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <div className="p-6 bg-slate-100 rounded-lg mx-4 space-y-4">
+      <h2 className="font-medium">Trending</h2>
+      <p className="font-bold">#InterMiami</p>
+      <p className="font-bold">#AlNassr</p>
+      <p className="font-bold">#GOAT</p>
+    </div>
+  );
+};
+
+export default RightSidebar;
---
+++ TwitterClone-master/client/src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+
+const firebaseConfig = {
+
+//PLEASE PASTE YOUR "firebaseConfig" HERE
+  apiKey: "AIzaSyB6GWk4AKDLUufF4n0SezC6JAefX4w6GDU",
+  authDomain: "twitter-clone-891f3.firebaseapp.com",
+  projectId: "twitter-clone-891f3",
+  storageBucket: "twitter-clone-891f3.appspot.com",
+  messagingSenderId: "634402472339",
+  appId: "1:634402472339:web:b202b18a47c2d70302e866"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
